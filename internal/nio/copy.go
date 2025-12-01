653c58a777074ef5e3dd243c6a409867ec2ef39a
tm4dev <tm4dev@gmail.com>
2025-12-01 13:33:31 +0700

    fix mem leak

--- internal/nio/copy.go
+++ internal/nio/copy.go
@@ -12,21 +12,30 @@ func CopyOnce(dst, src net.Conn, timeout time.Duration) int64 {
 	src.SetDeadline(time.Now().Add(timeout))
 	dst.SetDeadline(time.Now().Add(timeout))
 
-	done := make(chan struct{})
-	var n1, n2 int64
+	// Buffered channel to prevent goroutine leaks
+	done := make(chan int64, 2)
 
 	go func() {
-		n1, _ = io.Copy(dst, src)
-		done <- struct{}{}
+		n, _ := io.Copy(dst, src)
+		// Close the write side to signal the other direction to stop
+		if conn, ok := dst.(*net.TCPConn); ok {
+			conn.CloseWrite()
+		}
+		done <- n
 	}()
 
 	go func() {
-		n2, _ = io.Copy(src, dst)
-		done <- struct{}{}
+		n, _ := io.Copy(src, dst)
+		// Close the write side to signal the other direction to stop
+		if conn, ok := src.(*net.TCPConn); ok {
+			conn.CloseWrite()
+		}
+		done <- n
 	}()
 
-	// Wait for one direction to finish
-	<-done
+	// Wait for both directions to finish
+	n1 := <-done
+	n2 := <-done
 
 	return n1 + n2
 }
